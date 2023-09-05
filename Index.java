c27fe871f324a9e63d7acf2c3ad6c3dfbdb3d507
Victor <victorli1076@gmail.com>
2023-09-05 16:39:11 -0700

    fixed .isDirectory and .isHidden and finished init

--- Index.java
+++ Index.java
@@ -1,25 +1,60 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.FilenameFilter;
+import java.util.HashMap;
+import java.util.Stack;
 
 public class Index {
     protected String[] fileNames;
-    protected File[] files;
+    protected HashMap<String, Stack<String>> index;
 
     public Index() {
-        File folder = new File("C:\\Users\\helen\\Documents\\GitHub\\Honors Topics\\Git");
+        index = new HashMap<String, Stack<String>>();
+        File folder = new File(System.getProperty("user.dir"));
         System.out.println(folder.getAbsolutePath());
         fileNames = folder.list(new FilenameFilter() {
             @Override
             public boolean accept(File file, String name) {
-                return !name.startsWith(".") && file.isDirectory();
+                File currentFile = new File(name);
+                return !currentFile.isHidden() && !currentFile.isDirectory() && !name.equals("Index.txt");
             }
         });
     }
 
     public void init() {
+        File objects = new File("Objects");
+        objects.mkdir();
         for (int i = 0; i < fileNames.length; i++) {
+            File file = new File(fileNames[i]);
+            Blob blob = new Blob(file);
+            String hash = blob.hash;
+            if (index.containsKey(hash)) {
+                index.get(hash).push(fileNames[i]);
+            } else {
+                Stack<String> stack = new Stack<String>();
+                stack.push(fileNames[i]);
+                index.put(hash, stack);
+            }
+        }
 
+        try {
+            File indexFile = new File("Index.txt");
+            if (!File.createTempFile("Index", ".txt").exists()) {
+                indexFile.createNewFile();
+            }
+            FileWriter writer = new FileWriter(indexFile);
+            StringBuilder str = new StringBuilder();
+            for (String hash : this.index.keySet()) {
+                str.append(hash + " ");
+                for (String fileName : this.index.get(hash)) {
+                    str.append(fileName + " ");
+                }
+                str.append("\n");
+            }
+            writer.write(str.toString());
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("cant create Index.txt");
         }
     }
 }
--- Tester.java
+++ Tester.java
@@ -3,9 +3,7 @@ import java.io.File;
 public class Tester {
     public static void main(String[] args) {
         Index index = new Index();
-        for (String fileName : index.fileNames) {
-            System.out.println(fileName);
-        }
+        index.init();
 
     }
 }
